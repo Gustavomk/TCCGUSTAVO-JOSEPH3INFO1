ea5dab9c57f2f58e39ad7c68626b7222346c7e11
Joseph Barreto <48127907+JosephBarreto@users.noreply.github.com>
2020-10-06 14:37:44 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_SESSION['logado'])){
-    // se você possui algum cookie relacionado com o login deve ser removido
-    session_destroy();
-    header("location: localhost/tcc_da_quarentena/login.php");
-    exit();
-}
-?>
